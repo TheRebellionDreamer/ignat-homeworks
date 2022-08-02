6ee18f960f63f5d166f2ddde5f8e5bb02df6702d
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2022-08-02 17:05:34 +0300

    Create 5 homework

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import classes from './Header.module.css';
+import { PATH } from './Pages';
+
+const setActiveLink: ActiveLinkSetter = ({ isActive }) => {
+  return `${classes.link} ${isActive ? classes.active : ''}`;
+};
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+  const links: ILink[] = [
+    {
+      to: PATH.PRE_JUNIOR,
+      text: 'Pre-junior',
+    },
+    {
+      to: PATH.JUNIOR,
+      text: 'Junior',
+    },
+    {
+      to: PATH.JUNIOR_PLUS,
+      text: 'Junior+',
+    },
+  ];
 
-        </div>
-    )
+  return (
+    <header className={classes.header}>
+      <nav className={classes.navigation}>
+        {links.map(({ text, to }) => (
+          <NavLink key={to} to={to} children={text} className={setActiveLink} />
+        ))}
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+type ActiveLinkSetter = ({ isActive }: { isActive: boolean }) => string;
+
+interface ILink {
+  to: string;
+  text: string;
+}
+
+export default Header;
--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Error404 from './pages/Error404';
+import PreJunior from './pages/PreJunior';
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    // add paths
-}
+  PRE_JUNIOR: '/pre-junior',
+  JUNIOR: '/junior',
+  JUNIOR_PLUS: '/junior-plus',
+  ERROR_PAGE: '/*',
+};
 
 function Pages() {
-    return (
-        <div>
-            {/*Routes выбирает первый подходящий роут*/}
-            {/*<Routes>*/}
-
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                {/*<Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>*/}
-
-                {/*<Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>*/}
-                // add routes
-
-                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                {/*<Route path={'/*'} element={<Error404/>}/>*/}
-
-            {/*</Routes>*/}
-        </div>
-    )
+  return (
+    <div>
+      <Routes>
+        <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR} />} />
+        <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+        <Route path={PATH.ERROR_PAGE} element={<Error404 />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default Pages
+export default Pages;
